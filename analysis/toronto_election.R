d94227e730fcbf11fe95f2d635c048f57b035277
mroutley <matt@routleynet.org>
2014-03-08 15:14:19 -0500

    Summarise turnout stats

--- analysis/toronto_election.R
+++ analysis/toronto_election.R
@@ -4,7 +4,7 @@ election_results <- read.csv(file="data//election_results.csv")
 elections_df <- tbl_df(election_results)
 elections <- group_by(elections_df,year,candidate,ward)
 votes_by_year_ward <- elections_df %.%
-  group_by(year,ward,area) %.%
+  group_by(year,ward) %.%
   summarise(
     vote=sum(votes)
   )
@@ -17,8 +17,8 @@ candidates <- elections_df %.%
   filter(vote>5000) %.%
   arrange(year,desc(vote))
 
-qplot(ward,vote,data=votes, facets=~year)
-qplot(vote,data=votes, facets=~year)
+qplot(ward,vote,data=votes_by_year_ward, facets=~year)
+qplot(vote,data=votes_by_year_ward, facets=~year)
 
 turnout <- read.csv(file="data//turnout.csv")
 turnout_df <- tbl_df(turnout[,-1])
@@ -28,3 +28,10 @@ detail_turnout <- mutate(votes_eligible_by_ward_area_year,
                          turnout = vote/total_eligible,
                          spoiled = total_votes - vote) %.%
   filter(total_eligible > 0)
+
+turnout_by_ward_year <- merge(votes_by_year_ward,turnout_df) %.%
+  group_by(year,ward) %.%
+  summarise(eligible=sum(total_eligible),
+            votes=sum(vote)) %.%
+  mutate(turnout=votes/eligible)
+qplot(turnout,data=turnout_by_ward_year, facets=~year)
