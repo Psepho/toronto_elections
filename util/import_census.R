beaf9920e13b45132566822dca5989a87f3c82bb
mroutley <matt@routleynet.org>
2014-03-10 20:08:48 -0400

    More clean up

--- util/import_census.R
+++ util/import_census.R
@@ -1,4 +1,4 @@
-data <- read.csv(file="/data/98-316-XWE2011001-401.CSV")
+data <- read.csv(file="../data/98-316-XWE2011001-401.CSV")
 # Downloaded from http://www12.statcan.gc.ca/census-recensement/2011/dp-pd/prof/details/page.cfm?Lang=E&Geo1=CSD&Code1=3520005&Geo2=CD&Code2=3520&Data=Count&SearchText=toronto&SearchType=Begins&SearchPR=01&B1=All&Custom=&TABID=1
 library(dplyr)
 census_df <- tbl_df(data)
