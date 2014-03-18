935b36fe163169fa908e00f8126651b79d4ed224
Matthew Routley <matt@routleynet.org>
2014-03-18 10:27:26 -0400

    Update import_Excel.R
    Process candidate names, wards, and areas in R instead of later in Excel

--- util/import_Excel.R
+++ util/import_Excel.R
@@ -27,11 +27,14 @@ processFiles <- function(pattern) { # Takes a filename pattern and returns a dat
 fyear <- "2010"
 fpattern <- paste(fyear,".*_Mayor.xls?",sep="")  # pattern for filenames
 election_results <- processFiles(fpattern)
-
-rm(fpattern,fyear)
-write.csv(election_results, file="2003_election_results.csv")
-election_results <- rbind(read.csv("2003_election_results.csv"), read.csv("2006_election_results.csv"), read.csv("2010_election_results.csv"))
 election_results$candidate <- gsub(",","",tolower(election_results$candidate))
+election_results$ward <- gsub("Ward","",election_results$ward)
+election_results$area <- gsub("x","",election_results$area)
+write.csv(election_results, file=paste(fyear,"_election_results.csv",sep=""))
+          
+rm(fpattern,fyear)
+election_results <- rbind(read.csv("2003_election_results.csv"), read.csv("2006_election_results.csv"), read.csv("2010_election_results.csv"))
+
 write.csv(election_results[,-1], file="Data//election_results.csv")
 
 ###
