e1d0bbf6323ffe9e5c3564ccf9831ffa6aa1e49b
Matthew Routley <matt@routleynet.org>
2014-03-19 15:54:53 -0400

    Experimenting with ggmap

--- analysis/toronto_election.R
+++ analysis/toronto_election.R
@@ -2,8 +2,24 @@ library(dplyr)
 library(RSQLite)
 library(RSQLite.extfuns)
 library(ggplot2)
+library(ggmap)
 elections_db <- src_sqlite("data/elections_db.sqlite3")
 votes <- tbl(elections_db,"votes")
+turnout <- tbl(elections_db,"turnout")
+locations <- tbl(elections_db,"locations")
+
+turnout_geo <- as.data.frame(inner_join(turnout,locations, by=c("ward", "area","year")))
+
+turnout_geo <- tbl_df(turnout_geo) %.%
+  select(year,ward,area,long,lat,total_eligible,total_votes)
+turnout_2010 <- turnout_geo %.%
+  filter(year=="2010") %.%
+  mutate(turnout=total_votes/total_eligible)
+toronto_map <- qmap("queens park,toronto",zoom=11)
+toronto_map + geom_point(aes(x=long,y=lat,size=total_votes),data=turnout_2010)
+toronto_map + geom_point(aes(x=long,y=lat,size=turnout),data=turnout_2010)
+
+
 
 elections <- group_by(votes,year,candidate,ward)
 votes_by_year_ward <- votes %.%
@@ -44,7 +60,7 @@ candidates <- votes %.%
 qplot(ward,vote,data=votes_by_year_ward, facets=~year)
 qplot(vote,data=votes_by_year_ward, facets=~year)
 
-turnout <- tbl(elections_db,"turnout")
+
 
 turnout_by_year <- turnout %.%
   group_by(year) %.%
