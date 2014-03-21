fda5285dfa22c3b97d3e45dd2c0b22deec9bc38c
Matthew Routley <matt@routleynet.org>
2014-03-21 15:47:01 -0400

    Clean up and reorganize

--- analysis/toronto_election.R
+++ analysis/toronto_election.R
@@ -1,54 +1,41 @@
+#-----------
+# Data setup
+#-----------
+
+#Packages
 library(dplyr)
 library(RSQLite)
 library(RSQLite.extfuns)
 library(ggplot2)
-library(ggmap)
-elections_db <- src_sqlite("data/elections_db.sqlite3")
-votes <- tbl(elections_db,"votes")
-turnout <- tbl(elections_db,"turnout")
-locations <- tbl(elections_db,"locations")
-income <- tbl(elections_db,"family_income")
 
-median_income <- as.data.frame(income) %.%
+# Load the sqlite db and extract the tables
+elections_db <- src_sqlite("data/elections_db.sqlite3")
+votes <- as.data.frame(tbl(elections_db,"votes"))
+turnout <- as.data.frame(tbl(elections_db,"turnout"))
+locations <- as.data.frame(tbl(elections_db,"locations"))
+income <- as.data.frame(tbl(elections_db,"family_income"))
+
+#-----------
+# Summaries
+#-----------
+
+# Income
+median_income <- income %.%
   filter(income_type=="Median 2005 family income $",family_structure=="Total - All economic families") %.%
   select(SGC,value)
 
-turnout_geo <- as.data.frame(inner_join(turnout,locations, by=c("ward", "area","year")))
-
-turnout_geo <- tbl_df(turnout_geo) %.%
-  select(year,ward,area,long,lat,total_eligible,total_votes)
-turnout_2010 <- turnout_geo %.%
-  filter(year=="2010") %.%
-  mutate(turnout=total_votes/total_eligible)
-toronto_map <- qmap("queens park,toronto",zoom=11)
-toronto_map + geom_point(aes(x=long,y=lat,size=total_votes),data=turnout_2010)
-toronto_map + geom_point(aes(x=long,y=lat,size=turnout),data=turnout_2010)
-
-esri_import <- turnout_2010 %.%
-  select(long,lat,turnout)
-write.csv(esri_import,"esri_import.csv")
-
-setRepositories(addURLs =
-                  c(RStudio = "http://cran.rstudio.com/"))
-
-unzip("data//voting_location_2010_wgs84.zip"); library(maptools); library(gpclib); library(sp);
-gpclibPermit()
-
-
-
+# Votes and candidates
 elections <- group_by(votes,year,candidate,ward)
 votes_by_year_ward <- votes %.%
   group_by(year,ward) %.%
   summarise(
     vote=sum(votes)
   )
-
 votes_by_year <- votes %.%
   group_by(year) %.%
   summarise(
     vote=sum(votes)
   )
-
 areas_by_year <- votes %.%
   mutate(location=paste(ward,area,sep="-")) %.%
   select(year,location) %.%
@@ -56,14 +43,12 @@ areas_by_year <- votes %.%
   summarise(
     count=n_distinct(location)
   )
-
 candidates_by_year <- votes %.%
   select(year,candidate) %.%
   group_by(year) %.%
   summarise(
     count=n_distinct(candidate)
   )
-
 candidates <- votes %.%
   group_by(year,candidate) %.%
   summarise(
@@ -71,36 +56,47 @@ candidates <- votes %.%
   ) %.%
   filter(vote>5000) %.%
   arrange(year,desc(vote))
-
 qplot(ward,vote,data=votes_by_year_ward, facets=~year)
 qplot(vote,data=votes_by_year_ward, facets=~year)
 
-
-
+# Turnout
 turnout_by_year <- turnout %.%
   group_by(year) %.%
-  summarise(eligible=sum(total_eligible),
+  summarise(eligible=sum(total_eligible,na.rm=TRUE),
             votes=sum(total_votes)) %.%
   mutate(turnout=votes/eligible)
-
-
 votes_eligible_by_ward_area_year <- tbl_df(merge(votes_by_year_ward,turnout))
-
 detail_turnout <- mutate(votes_eligible_by_ward_area_year, 
                          turnout = vote/total_eligible,
                          spoiled = total_votes - vote) %.%
   filter(total_eligible > 0)
-
 turnout_year <- tbl_df(merge(votes_by_year_ward,turnout)) %.%
   group_by(year) %.%
   summarise(eligible=sum(total_eligible),
             votes=sum(vote)) %.%
   mutate(turnout=votes/eligible)
-
 turnout_by_ward_year <- merge(votes_by_year_ward,turnout) %.%
   group_by(year,ward) %.%
   summarise(eligible=sum(total_eligible),
             votes=sum(vote)) %.%
   mutate(turnout=votes/eligible)
-
 qplot(turnout,data=turnout_by_ward_year, facets=~year)
+
+#-----------
+# Mapping
+#-----------
+library(ggplot2)
+library(ggmap)
+
+# Start with a turnout map
+turnout_geo <- as.data.frame(inner_join(turnout,locations, by=c("ward", "area","year")))
+turnout_geo <- tbl_df(turnout_geo) %.%
+  select(year,ward,area,long,lat,total_eligible,total_votes)
+turnout_2010 <- turnout_geo %.% # Filter to 2010
+  filter(year=="2010") %.%
+  mutate(turnout=total_votes/total_eligible)
+toronto_map <- qmap("queens park,toronto",zoom=11)
+toronto_map + geom_point(aes(x=long,y=lat,size=turnout),data=turnout_2010)
+
+# Also plot total votes
+toronto_map + geom_point(aes(x=long,y=lat,size=total_votes),data=turnout_2010)
