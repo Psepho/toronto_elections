20a3c07c4bb9b17ee08373fed07d10a74f363c3c
Matthew Routley <matt@routleynet.org>
2014-03-25 15:57:37 -0400

    Fixed fill in mapping to standardize limits

--- analysis/maps.R
+++ analysis/maps.R
@@ -6,6 +6,9 @@ library(ggplot2)
 library(ggmap)
 library(maptools)
 
+# Standard base map
+toronto_map <- qmap("queens park,toronto",zoom=11, maptype = 'terrain')
+
 # Start with a turnout map
 turnout_geo <- as.data.frame(inner_join(turnout,locations, by=c("ward", "area","year")))
 turnout_geo <- tbl_df(turnout_geo) %.%
@@ -13,7 +16,7 @@ turnout_geo <- tbl_df(turnout_geo) %.%
 turnout_2010 <- turnout_geo %.% # Filter to 2010
   filter(year=="2010") %.%
   mutate(turnout=total_votes/total_eligible)
-toronto_map <- qmap("queens park,toronto",zoom=11, maptype = 'terrain')
+
 toronto_map + geom_point(aes(x=long,y=lat,size=turnout),colour="blue",data=turnout_2010)
 
 # Also plot total votes
@@ -27,7 +30,6 @@ positions_geo <- tbl_df(positions_geo) %.%
   select(year,ward,area,long,lat,weighted_votes,turnout)
 positions_2010 <- positions_geo %.% # Filter to 2010
   filter(year=="2010")
-toronto_map <- qmap("queens park,toronto",zoom=11, maptype = 'terrain')
 toronto_map + geom_point(aes(x=long,y=lat,colour=weighted_votes),data=positions_geo)+facet_wrap(~year)
 
 # 2010 results
@@ -46,8 +48,9 @@ data <- fortify(shapefile)
 data <-merge(data,shapefile@data,by.x="id",by.y="row.names")
 # Map the results
 p <- toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(weighted_votes,6)), alpha = 5/6, data=data) + 
-  scale_fill_brewer(palette ="PuOr",type="div","Left-Right Score")
+  geom_polygon(aes(x=long, y=lat, group=group, fill=weighted_votes), alpha = 5/6, data=data) + 
+  scale_fill_gradient2("Left-Right Score", midpoint = 0.55, mid = "white",limit=c(0.25,0.85))
+#scale_fill_brewer(palette ="PuOr",type="div","Left-Right Score")
 p
 
 # 2006 results
@@ -61,12 +64,13 @@ positions_2006 <- positions_geo %.%
   select(ward,area,weighted_votes) %.%
   mutate(ward_area=paste(ward,area,sep="_"))
 # Join positions and locations
-shapefile@data = data.frame(shapefile@data, positions_2006[match(shapefile$ward_area, positions_2010$ward_area),])
+shapefile@data = data.frame(shapefile@data, positions_2006[match(shapefile$ward_area, positions_2006$ward_area),])
 data <- fortify(shapefile)
 # Get the data back into the dataframe (this seems like it should be unneccessary)
 data <-merge(data,shapefile@data,by.x="id",by.y="row.names")
 # Map the results
 p <- toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(weighted_votes,6)), alpha = 5/6, data=data) + 
-  scale_fill_brewer(palette ="PuOr",type="div","Left-Right Score")
+  geom_polygon(aes(x=long, y=lat, group=group, fill=weighted_votes), alpha = 5/6, data=data) + 
+  scale_fill_gradient2("Left-Right Score", midpoint = 0.55, mid = "white",limit=c(0.25,0.85))
+  #scale_fill_brewer(palette ="PuOr",type="div","Left-Right Score")
 p
