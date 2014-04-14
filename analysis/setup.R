9dd7d10efa4ac097d7126f3b5d2e5ce8255e2cc1
Matthew Routley <matt@routleynet.org>
2014-04-14 15:17:03 -0400

    Moved scaffolding from maps.R

--- analysis/setup.R
+++ analysis/setup.R
@@ -1,22 +1,24 @@
 #-----------
-# Data setup
+# Packages
 #-----------
-
-#Packages
 library(dplyr)
 library(RSQLite)
 library(RSQLite.extfuns)
 library(ggplot2)
 library(reshape2)
 library(stringr)
-
+library(ggplot2)
+library(ggmap)
+library(maptools)
+#-----------
+# Data setup
+#-----------
 # tmp directory for downloads and unzips
 if(file.exists("tmp")) {
   # Nothing to do
 }  else {
   dir.create("tmp")
 }
-
 # Load the sqlite db and extract the tables
 elections_db <- src_sqlite("data/elections_db.sqlite3")
 votes <- as.data.frame(collect(tbl(elections_db,"votes")))
@@ -30,6 +32,14 @@ locations <- rbind(locations,locations %.%
 income <- as.data.frame(tbl(elections_db,"family_income"))
 # Positions
 candidate_positions <- as.data.frame(tbl(elections_db,"positions"))
+# Positions for just candidates in the upcoming election
+positions_2014 <- candidate_positions %.%
+  filter(year==2014) %.%
+  select(candidate, score) %.%
+  mutate(score=score/100)
+positions_2014 <- split(positions_2014$score, positions_2014$candidate)
+positions_2014 <- positions_2014[order(names(positions_2014))]
+# Positions by area
 positions <- as.data.frame(inner_join(votes,candidate_positions, by=c("candidate","year")))
 positions <- positions %.%
   group_by(year,ward,area) %.%
@@ -51,7 +61,17 @@ positions_geo <- positions_geo %.%
   arrange(year) %.%
   group_by(ward_area)%.%
   mutate(weighted_votes_change = c(0,diff(weighted_votes)))
-
+# Average positions for each ward_area
+positions_average_by_ward_area <- positions_geo %.%
+  group_by(ward,area,ward_area) %.%
+  summarise(position=mean(weighted_votes),votes=mean(total_votes)) %.%
+  arrange(ward,area)
+# Areas with votes in 2010
+active_areas_2010 <- positions_geo %.%
+  filter(year==2010,total_votes>0) %.%
+  select(ward,area,ward_area)
+active_areas_2010 <- active_areas_2010[,1:2]
+areas_for_2014 <- as.data.frame(inner_join(active_areas_2010,positions_average_by_ward_area, by=c("ward", "area")))
 # Scenarios
 scenario <- as.data.frame(tbl(elections_db,"scenario"))
 names(scenario)[3:4] <- c("candidate","total_votes") 
@@ -61,7 +81,6 @@ turnout_geo <- as.data.frame(inner_join(turnout,locations, by=c("ward", "area","
 turnout_geo <- tbl_df(turnout_geo) %.%
   select(year,ward,area,long,lat,total_eligible,total_votes) %.%
   mutate(turnout=total_votes/total_eligible,ward_area=paste(ward,area,sep="_"))
-
 # Active areas
 active_areas <- turnout_geo %.%
   select(year,ward,area,turnout)
@@ -75,15 +94,43 @@ turnout_in_active_areas <- tbl_df(merge(active_areas,turnout_geo))
 positions_in_active_areas <- tbl_df(merge(active_areas,positions_geo))
 active_areas <- active_areas %.%
   mutate(ward_area=paste(ward,area,sep="_"))
-
-
-# positions_votes <- as.data.frame(inner_join(positions_geo,positions[,c(2:3,5:6,11)], by=c("ward","area","year")))
-# positions_votes$score <- positions_votes$score/100
-# positions_votes$intensity <- (1-(positions_votes$weighted_votes-positions_votes$score)^2)
-# 
-# head(positions_votes)
-# qplot(weighted_votes, intensity, data = positions_votes, geom="point") + facet_grid(candidate~year) + xlab("Area position") + ylab("Voting intensity")
-# qplot(weighted_votes, total_votes, data = positions_votes, geom="point") + facet_grid(~year) + xlab("Area position") + ylab("Total votes")
-# qplot(intensity, total_votes, data = positions_votes, geom="point") + facet_grid(~year) + xlab("Intensity") + ylab("Total votes")
-#positions_geo$intensity <- positions_geo$total_votes*(1-(positions_geo$weighted_votes-0.6)^2)
-
+#-----------
+# Download shapefiles
+#-----------
+years <- c(2006,2010)
+for (year in years) {
+  if(file.exists(paste("tmp/subdivisions_",year,".zip",sep=""))) {
+    # Nothing to do
+  }  else {
+    download.file(paste("http://opendata.toronto.ca/gcc/voting_subdivision_",year,"_wgs84.zip",sep=""),
+                  destfile = paste("tmp/subdivisions_",year,".zip",sep=""))
+    unzip(paste("tmp/subdivisions_",year,".zip",sep=""), exdir="tmp")
+  }
+  shape <- paste("shapefile_",year,sep="")
+  file <- paste("tmp/VOTING_SUBDIVISION_",year,"_WGS84.shp",sep="")
+  assign(shape,readShapeSpatial(file, proj4string=CRS("+proj=longlat +datum=WGS84")))
+  rm(file,shape)
+}
+rm(year,years)
+#-----------
+# Standard base map
+#-----------
+toronto_map <- qmap("queens park,toronto",zoom=11, maptype = 'terrain')
+#---------
+# Shapefiles
+#---------
+# Extract the data object for each year and then combine into one data frame
+data_2010 <- fortify(shapefile_2010,region="AREA_NAME")
+data_2010$year <- as.integer(2010)
+data_2006 <- fortify(shapefile_2006,region="AREA_NAME")
+data_2006$year <- as.integer(2006)
+data_2003 <- fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 data
+data_2003$year <- as.integer(2003)
+data <- rbind(data_2010,data_2006,data_2003)
+rm(data_2003,data_2006,data_2010)
+data$ward_area <- paste(as.integer(str_sub(data$id,1,2)),as.integer(str_sub(data$id,-3,-1)),sep="_")
+data <- as.data.frame(inner_join(data,positions_geo[,-c(2:5)], by=c("ward_area","year")))
+# For 2014, assume same geography as 2010
+data_2014 <- fortify(shapefile_2010,region="AREA_NAME")
+data_2014$year <- as.integer(2014)
+data_2014$ward_area <- paste(as.integer(str_sub(data_2014$id,1,2)),as.integer(str_sub(data_2014$id,-3,-1)),sep="_")
