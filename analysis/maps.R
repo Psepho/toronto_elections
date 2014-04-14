e743651d7d4f627378c09b747905b4dce36fbbc3
Matthew Routley <matt@routleynet.org>
2014-04-14 15:16:38 -0400

    Moved more scaffolding to setup.R

--- analysis/maps.R
+++ analysis/maps.R
@@ -1,32 +1,4 @@
 source("analysis//setup.R")
-#-----------
-# Packages
-#-----------
-library(ggplot2)
-library(ggmap)
-library(maptools)
-#-----------
-# Download shapefiles
-#-----------
-years <- c(2006,2010)
-for (year in years) {
-  if(file.exists(paste("tmp/subdivisions_",year,".zip",sep=""))) {
-    # Nothing to do
-  }  else {
-    download.file(paste("http://opendata.toronto.ca/gcc/voting_subdivision_",year,"_wgs84.zip",sep=""),
-                  destfile = paste("tmp/subdivisions_",year,".zip",sep=""))
-    unzip(paste("tmp/subdivisions_",year,".zip",sep=""), exdir="tmp")
-  }
-  shape <- paste("shapefile_",year,sep="")
-  file <- paste("tmp/VOTING_SUBDIVISION_",year,"_WGS84.shp",sep="")
-  assign(shape,readShapeSpatial(file, proj4string=CRS("+proj=longlat +datum=WGS84")))
-  rm(file,shape)
-}
-rm(year,years)
-#-----------
-# Standard base map
-#-----------
-toronto_map <- qmap("queens park,toronto",zoom=11, maptype = 'terrain')
 #---------
 # Point maps
 #---------
@@ -36,20 +8,6 @@ point_map <- function(data,variable){
 point_map(turnout_geo,"turnout")
 point_map(turnout_geo,"total_votes")
 point_map(positions_geo,"weighted_votes")
-#---------
-# Shapefiles
-#---------
-# Extract the data object for each year and then combine into one data frame
-data_2010 <- fortify(shapefile_2010,region="AREA_NAME")
-data_2010$year <- as.integer(2010)
-data_2006 <- fortify(shapefile_2006,region="AREA_NAME")
-data_2006$year <- as.integer(2006)
-data_2003 <- fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 data
-data_2003$year <- as.integer(2003)
-data <- rbind(data_2010,data_2006,data_2003)
-rm(data_2003,data_2006,data_2010)
-data$ward_area <- paste(as.integer(str_sub(data$id,1,2)),as.integer(str_sub(data$id,-3,-1)),sep="_")
-data <- as.data.frame(inner_join(data,positions_geo[,-c(2:5)], by=c("ward_area","year")))
 #---------
 # Map the results
 #---------
@@ -77,13 +35,6 @@ toronto_map +
   scale_fill_gradient("Change in position", low="white", high="red", space="Lab")+ 
   facet_wrap(~year)
 
-
-data_2014 <- fortify(shapefile_2010,region="AREA_NAME")
-data_2014$year <- as.integer(2014)
-data_2014$ward_area <- paste(as.integer(str_sub(data_2014$id,1,2)),as.integer(str_sub(data_2014$id,-3,-1)),sep="_")
-scenario_geo <- tbl_df(scenario) %.%
-  mutate(ward_area=paste(ward,area,sep="_"))
-data <- as.data.frame(inner_join(data_2014,scenario_geo[,-c(1:2)], by=c("ward_area")))
 toronto_map +
   geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(total_votes,length=200)), alpha = 5/6, data=data) +
   scale_fill_brewer("Votes") + 
