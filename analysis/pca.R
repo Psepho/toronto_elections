a1084b25e4642fb55726ed124d47d07f73890bb4
Matthew Routley <matt@routleynet.org>
2014-04-15 16:04:13 -0400

    Extend the model to consider more variables

--- analysis/pca.R
+++ analysis/pca.R
@@ -6,11 +6,15 @@ locations <- locations %.%
   mutate(ward_area=paste(ward,area,sep="_"),GEO=as.character(CTUID))
 locations <- locations[,c(12:13)]
 locations$GEO <- as.factor(locations$GEO)
+locations <- inner_join(locations,positions_average_by_ward_area, by=c("ward_area")) %.%
+  group_by(GEO) %.%
+  summarize(position=mean(position))
 library(MASS)
 require(akima)
 require(mgcv)
 
-model <- (~ commuting_duration + income + median_age)
+#model <- (~ commuting_duration + income + median_age + median_home_value + public_transit + children)
+model <- (~ median_age + median_home_value + public_transit + children)
 pca <- princomp(model, data = census_summary, na.action = na.omit, cor = TRUE)
 summary(pca)
 loadings(pca)
@@ -21,7 +25,20 @@ model_predictions <- predict(pca)
 merged_data <- merge(census_summary, model_predictions, by="row.names", all.x = TRUE, sort = FALSE)
 merged_data <- merged_data[,-1]
 merged_data <- inner_join(merged_data,locations, by=c("GEO"))
-merged_data <- inner_join(merged_data,positions_average_by_ward_area, by=c("ward_area"))
 
 position_model <- lm(position ~ Comp.1 + Comp.2, data = merged_data)
 summary(position_model)
+
+plot(pca$scores[,1:2], main = "Position score", xlab = "Comp1", ylab = "Comp2")
+contour(interp(pca$scores[,1], pca$scores[,2], fitted(position_model), duplicate = "mean"), add = TRUE, col = "red", labcex = 0.8)
+AbilityContour <- merge(MuniPCA$scores, as.data.frame(fitted(AbilityModel)), by = "row.names")
+AbilityContour <- AbilityContour[,-1]
+names(AbilityContour)[5] <- "fitted"
+contour(interp(AbilityContour[,1], AbilityContour[,2], AbilityContour$fitted, duplicate = "mean"), add = TRUE, col = "blue", labcex = 0.8)
+
+
+predict.pca <- predict(pca)
+eqscplot(model_predictions)
+
+# median of over 18
+# count of kids
