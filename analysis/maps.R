df53e1a43c2881c831bfcc7567e8b5c0a8394e3f
Matthew Routley <matt@routleynet.org>
2014-04-17 15:13:49 -0400

    Change data name to geo

--- analysis/maps.R
+++ analysis/maps.R
@@ -3,7 +3,7 @@ source("analysis//setup.R")
 # Point maps
 #---------
 point_map <- function(data,variable){
-  toronto_map + geom_point(aes_string(x="long",y="lat",colour=variable),data=data) + facet_wrap(~year)
+  toronto_map + geom_point(aes_string(x="long",y="lat",colour=variable),data=geo) + facet_wrap(~year)
 }
 point_map(turnout_geo,"turnout")
 point_map(turnout_geo,"total_votes")
@@ -13,13 +13,13 @@ point_map(positions_geo,"weighted_votes")
 #---------
 # Positions
 toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(weighted_votes, length=0.15)), alpha = 5/6, data=data) + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(weighted_votes, length=0.15)), alpha = 5/6, data=geo) + 
   scale_fill_brewer("Position", type="div") +   
   #scale_fill_gradient2("Left-Right Score", midpoint = median(data$weighted_votes), mid = "white",limit=c(0.25,0.85)) +
   facet_wrap(~year)
 # Turnout
 toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(turnout,length = 0.25)), alpha = 5/6, data=data) + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(turnout,length = 0.25)), alpha = 5/6, data=geo) + 
   scale_fill_brewer("Turnout") + 
   facet_wrap(~year)
 # Change in position
@@ -36,7 +36,7 @@ toronto_map +
   facet_wrap(~year)
 
 toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(total_votes,length=200)), alpha = 5/6, data=data) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(total_votes,length=200)), alpha = 5/6, data=geo) +
   scale_fill_brewer("Votes") + 
   facet_wrap(~candidate)
 
