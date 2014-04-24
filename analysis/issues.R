72f35e6b0ec95d4f32bcf6786ffae0e83f937b8a
Matthew Routley <matt@routleynet.org>
2014-04-24 14:13:07 -0400

    Predict votes and summarize by proportion per ward

--- analysis/issues.R
+++ analysis/issues.R
@@ -6,27 +6,53 @@ source("analysis//setup.R")
 
 issues_df <- positions  %.%
   group_by(ward, area, candidate, add = FALSE) %.%
-  summarize(votes=sum(votes), transportation=mean(Transportation), life_environment=mean(Life_environment), finance_budget=mean(Finance_Budget), waste_management=mean(Waste_management))
+  summarize(votes=sum(votes), airport_expansion=mean(`Airport expansion`), finance_budget=mean(`Finance & Budget`), transit=mean(Transit), transportation=mean(Transportation), waste_management=mean(`Waste management`))
 issues_df$ward <- as.factor(issues_df$ward)
+issues_df <- group_by(issues_df[,c(1,4:9)], ward)
+areas_per_ward <- areas_for_2014 %.%
+  group_by(ward) %.%
+  summarize(count=n())
 
 # Issue models for each ward ----------------------------------------------
-
+issues_formula <- votes ~ transportation + transit + finance_budget + waste_management + airport_expansion
 library(plyr)
 library(dplyr)
 issues_model <- function(df){
-  lm(votes ~ transportation + life_environment + finance_budget + waste_management, data = df)
+  lm(issues_formula, data = df, na.action = na.omit)
 }
 issues_by_ward <- dlply(issues_df, .(ward), issues_model)
 coefs_by_ward <- ldply(issues_by_ward, function(x) coef(x))
 
 # Predict votes from position scores --------------------------------------
+names(candidate_positions)[3:7] <- names(issues_df)[3:7]
+predicted_votes <- function(pred_values) {
+  output <- ldply(issues_by_ward, function(x) predict(x, pred_values, interval = "confidence"))
+  names(output)[2] <- "votes"
+  output
+}
 
-#Not sure if this is necessary, does it match by names?
-# names(candidate_positions)[4:7] <- names(issues_df)[c(7,5,8,6)]
-pred_values <- issues_df[1,5:8] # Position scores
-predicted_votes <- ldply(issues_by_ward, function(x) predict(x, pred_values, interval = "confidence"))
+# Collect votes by candidate ----------------------------------------------
+library(plyr)
+library(dplyr)
+results <- data.frame(candidate = NA, ward = 0, votes = NA, lwr = NA, upr = NA)
+candidates <- c(1,3,13,14,16)
+for(candidate in candidates) {
+  pred_values <- candidate_positions[candidate,] # Position scores
+  results <- rbind(results, data.frame(candidate=candidate_positions[candidate,1],predicted_votes(pred_values)))
+}
+detach("package:plyr", unload=TRUE)
+results <- results[-1,]
+results$ward <- as.integer(results$ward)
+#results <- left_join(results, areas_per_ward, by = "ward")
+#results$adj_votes <- results$votes * results$count
+prop.table(tapply(results$votes, results[1:2], sum),2)
+prop.table(tapply(results$votes, results[1], sum))
 
-#issues_by_ward.dplyr <- do(issues_df, failwith(NULL, lm), formula = votes ~ transportation + life_environment + finance_budget + waste_management)
+
+# dplyr attempt -----------------------------------------------------------
+
+issues_df.dplyr <- group_by(issues_df[,c(1,4:9)], ward)
+issues_by_ward.dplyr <- do(issues_df.dplyr, failwith(NULL, lm), formula = issues_formula)
 
 # Archived stuff ----------------------------------------------------------
 # library(biglm)
