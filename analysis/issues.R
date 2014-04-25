55c2855434ab17334fafd1c5003e1b23af799678
Matthew Routley <matt@routleynet.org>
2014-04-25 16:49:58 -0400

    Change legend labels to descriptors

--- analysis/issues.R
+++ analysis/issues.R
@@ -68,7 +68,7 @@ issue_map <- function(output) { # Plot the results on a map by ward
   levels(data$candidate) <- unlist(strsplit(candidate_labels, split=" "))
   toronto_map +
     geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(adj_votes, n = 8)), alpha = 5/6, data=data) +
-    scale_fill_brewer("Votes") + 
+    scale_fill_brewer("Votes", labels=c("Low", rep("",6), "High")) + 
     facet_wrap(~candidate)
 }
 output_major <- results %.%
@@ -79,7 +79,7 @@ names(coefs_melt)[2:3] <- c("coefficient", "beta")
 data <- as.data.frame(inner_join(geo_2014,coefs_melt, by=c("ward")))
 toronto_map +
   geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(beta,7)), alpha = 5/6, data=data) +
-  scale_fill_brewer("Votes", type="div") + 
+  scale_fill_brewer("Position", type="div", labels=c("Left", rep("",5), "Right")) + 
   facet_wrap(~coefficient)
 
 # dplyr attempt -----------------------------------------------------------
@@ -95,9 +95,14 @@ issues_by_ward.dplyr <- do(issues_df.dplyr, failwith(NULL, lm), formula = issues
 #   summarize(votes=sum(votes), transportation=mean(Transportation), life_environment=mean(Life_environment), finance_budget=mean(Finance_Budget), waste_management=mean(Waste_management))
 # issues_df$ward <- as.factor(issues_df$ward)
 # 
-# i_model <- votes ~ transportation + life_environment + finance_budget + waste_management
-# issues_model <- lm(i_model, data=issues_df, na.action = na.omit)
-# summary(issues_model)
-# issues_model_ward <- update(issues_model, . ~ (.):ward)
-# summary(issues_model_ward)
+issues_df <- positions  %.%
+  group_by(year, candidate, ward, area, add = FALSE) %.%
+  summarize(votes=sum(votes), airport_expansion=mean(`Airport expansion`), finance_budget=mean(`Finance & Budget`), transit=mean(Transit), transportation=mean(Transportation), waste_management=mean(`Waste management`))
+issues_df$year <- as.factor(issues_df$year)
+#issues_df <- group_by(issues_df[,c(1,4:9)], ward)
+i_model <- votes ~ transportation + life_environment + finance_budget + waste_management
+issues_model <- lm(issues_formula, data=issues_df, na.action = na.omit)
+summary(issues_model)
+issues_model_ward <- update(issues_model, . ~ (.):year)
+summary(issues_model_ward)
 # anova(issues_model,issues_model_ward)
