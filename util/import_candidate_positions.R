ea85711b47d6067080ce1ae2298fcb8d30ebcebc
Matthew Routley <matt@routleynet.org>
2014-04-28 15:17:41 -0400

    Create import_candidate_positions.R

---
+++ util/import_candidate_positions.R
@@ -0,0 +1,24 @@
+#----------
+# Imports the candidate position excel file
+# Outputs candidate_positions.RData
+#----------
+library(XLConnect)
+drop_topic <- "Life & environment" # Drop this topic
+positions_excel <- readWorksheet(object=loadWorkbook("data//Position Scores.xlsx"), "Detailed Postions", startRow=3)
+names(positions_excel) <- tolower(names(positions_excel))
+positions_excel$candidate <- tolower(positions_excel$candidate)
+positions_excel$year <- as.factor(positions_excel$year)
+candidate_positions <- positions_excel %.%
+  mutate(topic=topic.type) %.%
+  group_by(candidate, year, topic) %.%
+  summarize(score=mean(score)) %.%
+  filter(topic!=drop_topic)
+candidate_scores <- positions_excel %.% # Generate summary left-right score
+  filter(topic!=drop_topic) %.% # Drop this topic
+  group_by(candidate, year) %.%
+  summarize(score=mean(score))
+candidate_positions <- melt(candidate_positions)
+candidate_positions <- dcast(candidate_positions, candidate + year ~ topic)
+candidate_positions <- inner_join(candidate_positions,candidate_scores, by=c("candidate","year"))
+rm(positions_excel, candidate_scores)
+save(candidate_positions, file = "data/candidate_positions.RData")
