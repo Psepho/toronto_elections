fbabf7a8e7ff468c67134a80003f85bed6c509cf
Matthew Routley <matt@routleynet.org>
2014-05-27 16:14:47 -0400

    Small adjustments to contour format

--- analysis/issues_surface.R
+++ analysis/issues_surface.R
@@ -26,20 +26,21 @@ models <- models %>% group_by(ward)
 
 # Predict votes for positions ---------------------------------------------
 
-issue_resolution <- 20.0
+issue_resolution <- 10.0
 issue_vector <- as.integer(seq(0, 100, issue_resolution))
 issue_universe <- expand.grid(airport_expansion = issue_vector, finance_budget = 50, transit = 50, transportation = issue_vector, waste_management = 50, ward = levels(issues_df$ward))
 issue_universe <- issue_universe %>% group_by(airport_expansion, finance_budget, transit, transportation, waste_management, ward)
 predictions <- issue_universe %>% do(votes = predict(models$mod[models$ward==.$ward][[1]], ., interval = "confidence")[1])
 predictions$votes <- unlist(predictions$votes)
+rm(issue_resolution, issue_vector, issue_universe)
 
 
 # Plot contour maps -------------------------------------------------------
 
 library(ggplot2)
 
-p <- ggplot(filter(predictions), aes(airport_expansion, transportation, z = votes))
+p <- ggplot(predictions, aes(airport_expansion, transportation, z = votes))
 p + 
-  stat_contour(binwidth = 10, size = 1, aes(colour = ..level..)) +
-  scale_colour_gradient2() +
+  stat_contour(binwidth = 5, size = 1.5, aes(colour = ..level..)) +
+  scale_colour_gradient2("Votes", labels=c("Low", rep("", 2), "High"), low = "white") +
   facet_wrap(~ward)
