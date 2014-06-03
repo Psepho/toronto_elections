2005b7e818d50a0968a3bccca261dcef964ba3b8
Matthew Routley <matt@routleynet.org>
2014-06-03 11:05:40 -0400

    Use all years for PCA

--- analysis/pca.R
+++ analysis/pca.R
@@ -15,7 +15,7 @@ vote_history$CTUID <- as.factor(vote_history$CTUID)
 vote_history <- vote_history %.%
   group_by(year, ward, area, CTUID) %.%
   summarize(area_position = mean(weighted_votes))
-census_toronto <- inner_join(census_summary,vote_history, by=c("year", "CTUID"))
+census_toronto <- inner_join(census_summary,vote_history, by=c("CTUID"))
 census_toronto$prop_children <- census_toronto$children/census_toronto$pop
 
 # Create PCA model --------------------------------------------------------
@@ -33,7 +33,7 @@ biplot(pca, c(1,3), scale = TRUE, main = model)
 model_predictions <- predict(pca)
 merged_data <- merge(census_toronto, model_predictions, by="row.names", all.x = TRUE, sort = FALSE)
 merged_data <- merged_data[,-1]
-position_model <- lm(area_position ~ Comp.1 + Comp.2 + Comp.3, data = merged_data)
+position_model <- lm(area_position ~ Comp.1 + Comp.2 + Comp.3 + Comp.4, data = merged_data)
 summary(position_model)
 
 # Map components ----------------------------------------------------------
