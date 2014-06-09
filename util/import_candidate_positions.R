aaa2fd82bdeda94191d2821a719159696846c37a
Matthew Routley <matt@routleynet.org>
2014-06-09 13:37:29 -0400

    Drop NAs

--- util/import_candidate_positions.R
+++ util/import_candidate_positions.R
@@ -13,12 +13,12 @@ positions_excel$year <- as.factor(positions_excel$year)
 candidate_positions <- positions_excel %.%
   mutate(topic=topic.type) %.%
   group_by(candidate, year, topic) %.%
-  summarize(score=mean(score)) %.%
+  summarize(score=mean(score, na.rm = TRUE)) %.%
   filter(topic!=drop_topic)
 candidate_scores <- positions_excel %.% # Generate summary left-right score
   filter(topic!=drop_topic) %.% # Drop this topic
   group_by(candidate, year) %.%
-  summarize(left_right_score=mean(score))
+  summarize(left_right_score=mean(score, na.rm = TRUE))
 candidate_positions <- melt(candidate_positions)
 candidate_positions <- dcast(candidate_positions, candidate + year ~ topic)
 candidate_positions <- inner_join(candidate_positions,candidate_scores, by=c("candidate","year"))
